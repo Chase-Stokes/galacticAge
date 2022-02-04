5b9b5d1f39f5400125825ae0d8819a2532180053
Chase Stokes <chasehunterstokes@gmail.com>
2022-02-04 09:18:09 -0800

    add a test for the constructor, good fail

---
+++ __tests__/age.test.js
@@ -0,0 +1,13 @@
+import UserLife from "../src/age.js";
+
+describe('UserLife', () => {
+
+  let user;
+  beforeEach(() => {
+    user = new UserLife(10, 80);
+  })
+
+  test('should create an object with the properties of the constructor', () => {
+    expect(user).toEqual({"age": 10, "lifeExpectancy": 80})
+  });
+})
